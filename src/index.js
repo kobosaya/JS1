94b899140bb78167198a2ff6ab8a53984c3a4ae4
kobosaya <121376286+kobosaya@users.noreply.github.com>
2023-01-09 22:39:31 +0900

    セクション4-23

--- src/index.js
+++ src/index.js
@@ -76,12 +76,52 @@
 // console.log(func1("func1です"));
 
 //アロー(arow)関数  ※()の中が引数　()を省略することも可能。保存すると()がつく。{}とreturnを省略することもできる。
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "こぼりん",
+//   age: 26
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+// // cf.テンプレート文字列
+
+// ↓オブジェクトNEW
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+// // ↑これがNEW　コードが短くて済む。簡潔。Reactでもよく使う。オブジェクトだけでなく配列にも使える↓
+
+// ↓配列
+// const myProfile = ["こぼりん", 26];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// //　↓配列NEW　※順番が異なるので注意
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など　※Reactでよく使う
+ */
+// nameだけでは undefinedとなるので、name="ゲスト"と入れてデフォルト化（初期値）する。
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("こぼりん");
+
+/**
+ * スプレッド構文 ...　ややこいらしい
+ */
+//配列の展開
